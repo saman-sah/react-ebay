e48631e49eb92b9b75935bf1c0f8b09401c3c168
saman-sah <sahraei.saman91@gmail.com>
2024-08-17 12:34:49 +0330

    update product

--- app/product/[id]/page.js
+++ app/product/[id]/page.js
@@ -31,9 +31,37 @@ export default function Product({ params }) {
                   Condition: <span className='font-bold text-[17px] ml-2'>New</span>
                 </div>
               </div>
+              <div className='border-b py-1' />
+              <div className='pt-3'>
+                <div className='w-full flex items-center justify-between'>
+                  <div className='flex items-center'>
+                    Price:
+                    {
+                      product?.price
+                        ? <div className='font-bold text-[20px] ml-2'>
+                          GBP {(product?.price / 100).toFixed(2)}
+                        </div>
+                        : null
+                    }
+                  </div>
+                </div>
+                <button className='bg-[#3498c9] text-white py-2 px-20 rounded-full cursor-pointer'>
+                  Add to Cart
+                </button>
+                <div className='border-b py-1' />
+                <div className='pt-3'>
+                  <div className='font-semibold pb-1'>
+                    Description
+                  </div>
+                  <div className='text-sm'>
+                    {product?.description}
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
+        <SimilarProducts />
       </MainLayout>
     </>
   )
