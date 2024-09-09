1b3d3e668499c157da11a68c3973b58cb2dbd115
saman-sah <sahraei.saman91@gmail.com>
2024-09-09 18:22:05 +0330

    update cart

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,17 +1,34 @@
 "use client"
 
-import MainLayout from "../layouts/MainLayout"
-import SimilarProducts from "../components/SimilarProducts"
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+
+import { useCart } from '../context/cart'
+
 import CartItem from '../components/CartItem'
+import MainLayout from "../layouts/MainLayout"
+import ClientOnly from "../components/ClientOnly"
+import useIsLoading from '../hooks/useIsLoading'
+import SimilarProducts from "../components/SimilarProducts"
 
 export default function Cart() {
-  const product = {
-    id: 1,
-    title: 'Brown Leather Bag',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum',
-    url: 'https://picsum.photos/id/7',
-    price: 2500
+  const cart = useCart()
+  const router = useRouter()
+
+  useEffect(() => {
+    useIsLoading(true)
+    cart.getCart()
+    cart.cartTotal()
+    useIsLoading(false)
+  }, [cart])
+
+  const goToCheckout = () => {
+    if (!Cart.cartTotal()) {
+      alert("You dont have any items in the cart")
+      return
+    }
   }
+
   return (
     <>
       <MainLayout>
@@ -20,38 +37,48 @@ export default function Cart() {
             Shopping Cart
           </div>
           <div className='relative flex items-baseline justify-between gap-2'>
-            <div className='w-[65%]'>
-              <CartItem
-                key={product.id}
-                product={product}
-              />
-            </div>
+            <ClientOnly>
+              <div className='w-[65%]'>
+                {
+                  cart.getCart().map(product => {
+                    <CartItem
+                      key={product.id}
+                      product={product}
+                    />
+                  })
+                }
+              </div>
+            </ClientOnly>
             <div
               id='GoToCheckout'
               className='md-w-[33%] absolute top-0 m-2 right-0'
             >
-              <div className='bg-white p-4 border'>
-                <button className='flex items-center justify-between bg-blue-600 w-full text-white font-semibold p-3 rounded-full mt-4'>
-                  Go to Checkout
-                </button>
+              <ClientOnly>
+                <div className='bg-white p-4 border'>
+                  <button
+                    onClick={() => goToCheckout()}
+                    className='flex items-center justify-between bg-blue-600 w-full text-white font-semibold p-3 rounded-full mt-4'>
+                    Go to Checkout
+                  </button>
 
-                <div className='flex items-center justify-between mt-4 text-sm mb-1'>
-                  <div>Items (3)</div>
-                  <div>12.99</div>
+                  <div className='flex items-center justify-between mt-4 text-sm mb-1'>
+                    <div>Items ({cart.getCart().length})</div>
+                    <div>${(cart.cartTotal() / 100).toFixed(2)}</div>
+                  </div>
+
+                  <div className='flex items-center justify-between mt-4 text-sm mb-1'>
+                    <div>Shipping</div>
+                    <div>Free</div>
+                  </div>
+
+                  <div className='border-b border-gray-300' />
+
+                  <div className='flex items-center justify-between mt-4 mb-1 text-lg font-semibold'>
+                    <div>Subtotal</div>
+                    <div>${(cart.cartTotal() / 100).toFixed(2)}</div>
+                  </div>
                 </div>
-
-                <div className='flex items-center justify-between mt-4 text-sm mb-1'>
-                  <div>Shipping</div>
-                  <div>Free</div>
-                </div>
-
-                <div className='border-b border-gray-300' />
-
-                <div className='flex items-center justify-between mt-4 mb-1 text-lg font-semibold'>
-                  <div>Subtotal</div>
-                  <div>12.99</div>
-                </div>
-              </div>
+              </ClientOnly>
             </div>
           </div>
         </div>
